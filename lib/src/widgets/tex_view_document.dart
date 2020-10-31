8e9f43c0e279d26cb98328062085a8ad8da39cf5
ustchcl <ustchcl@gmail.com>
2020-10-31 18:09:10 +0800

    [U] dotted

--- lib/src/widgets/tex_view_document.dart
+++ lib/src/widgets/tex_view_document.dart
@@ -3,15 +3,22 @@ import 'package:flutter_tex/src/models/tex_view_widget_meta.dart';
 import 'package:flutter_tex/src/utils/style_utils.dart';
 
 class TeXViewDocument extends TeXViewWidget {
-  final String id;
+  String id;
 
   /// Raw String containing HTML and TEX Code e.g. r"""$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$<br> """
-  final String data;
+  String data;
 
   /// Style TeXView Widget with [TeXViewStyle].
-  final TeXViewStyle style;
+  TeXViewStyle style;
 
-  const TeXViewDocument(this.data, {this.id, this.style});
+  TeXViewDocument(String data, {String id, TeXViewStyle style}) {
+    this.id = id;
+    this.style = style;
+    this.data = data
+        .replaceAll(new RegExp(r"/&lt;dot&gt;/g"),
+            '<font class="question_dot_underline">')
+        .replaceAll(new RegExp(r"/&lt;\/dot&gt;/g"), "</font>");
+  }
 
   @override
   TeXViewWidgetMeta meta() {
